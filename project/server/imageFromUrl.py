1b03c955469cd70bd39b03d2c174dff6407e1c8a
Ubuntu <ubuntu@ip-172-31-22-85.ap-southeast-1.compute.internal>
2017-11-04 07:33:50 +0000

    hings here t# Please enter the commit message for your changes. Lines starting

---
+++ project/server/imageFromUrl.py
@@ -0,0 +1,26 @@
+import urllib2
+import sys
+import shutil
+
+
+def downloadImage(urlFile, downloadPath):
+	if downloadPath and not downloadPath.endswith('/'): downloadPath += '/'
+	f = open(urlFile,'r')
+	i = 0
+ 	while True:
+		fileName = f.readline()
+		if not fileName: break
+		if fileName <> '\n' :
+			i = i + 1
+			print("Downloading {}-th image, from {}, to {}{}.jpg".format(i,fileName,downloadPath,i))
+			try:
+				request = urllib2.urlopen("{}".format(fileName),timeout = 5)
+				with open("{}{}.jpg".format(downloadPath,i),"w") as ff:
+					ff.write(request.read())
+				#urllib.urlretrieve("{}".format(fileName), "{}{}.jpg".format(downloadPath,i))
+			except:
+				print('fail to download')	
+#			shutil.move("{}.jpg".format(i),"{}{}.jpg".format(downloadPath,i))
+	f.close()
+#fileList = "urlFile.txt"
+#downloadImage(fileList,"/home/ubuntu/project/")
